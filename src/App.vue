9c9f569f5b18acf1f9f2209ed8efe4ed5bcf8c88
Kadin Zhang <cykadin@gmail.com>
2018-12-19 18:38:59 -0500

    strip vue-cli presets

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <h1>Image classification using MobileNet</h1>
     <p>
