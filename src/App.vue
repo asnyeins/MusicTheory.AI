a2e865894c78401f3a44d91a2858f16a567629e1
Kadin Zhang <cykadin@gmail.com>
2018-12-21 15:58:28 -0500

    import magenta models

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,7 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <h1>Image classification using MobileNet</h1>
-    <p>
-      The MobileNet model labeled this as
-      <span id="result">...</span> with a confidence of
-      <span id="probability">...</span>
-    </p>
-    <img
-      crossorigin="anonymous"
-      src="https://ml5js.org/docs/assets/img/bird.jpg"
-      id="image"
-      width="400"
-    >
-  </div>
+    <div id="app">
+        <Transcribe/>
+    </div>
 </template>
 
 <script>
@@ -25,42 +13,15 @@ Image Recognition:
 -Image colorization (modeldepot) **
 -Emotion Classification
 */
-import HelloWorld from "./components/HelloWorld.vue";
+import Transcribe from "./components/Transcribe.vue";
 
 export default {
-  name: "app",
-  components: {
-    HelloWorld
-  },
-  mounted: () => {
-    const image = document.getElementById("image");
-    // The result tag in the HTML;
-    const result = document.getElementById("result");
-    // The probability tag in the HTML;
-    const probability = document.getElementById("probability");
-
-    // Initialize the Image Classifier method with MobileNet;
-    const classifier = ml5.imageClassifier("MobileNet", function() {
-      console.log("Model Loaded!");
-    });
-
-    // Make a prediction with the selected image;
-    // This will return an array with a default of 10 options with their probabilities;
-    classifier.predict(image, function(err, results) {
-      result.innerText = results[0].className;
-      probability.innerText = results[0].probability.toFixed(4);
-    });
-  }
+    name: "app",
+    components: {
+        Transcribe
+    }
 };
 </script>
 
 <style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/Transcribe.vue
@@ -0,0 +1,24 @@
+<template>
+    <h1>hey</h1>
+</template>
+
+<script>
+import * as mm from "@magenta/music";
+
+export default {
+    name: "Transcribe",
+    data() {
+        return {
+            modelReady: false
+        };
+    },
+    mounted: function() {},
+    methods: {
+        async transcribeFile(file) {}
+    }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
